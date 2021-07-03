7268317aa87fa5c2cd26fd7e523004112aac79c4
Shourav Nath <shourav.nath@priyo.com>
2021-07-03 22:32:11 +0600

    new requirements

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ AWS.config.update({
 var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
+function update_user(user) {
+  var paramsToUpdate = {
+    TableName: 'UserTable',
+    Key:{
+      "email": user['email'],
+      "registration_date": user['registration_date']
+    },
+    UpdateExpression: "set balance = :balance, user_name=:name, queries=:queries, active=:active, credit=:credit",
+    ExpressionAttributeValues:{
+        ":credit": user['credit'],
+        ":balance": user['balance'],
+        ":name": user['user_name'],
+        ":active": user['active'],
+        ":queries": user['queries']
+    },
+    ReturnValues:"UPDATED_NEW"
+  }
+  docClient.update(paramsToUpdate, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+        return false
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+        return true
+    }
+  });
+}
+
 
 app.get('/users', (req, res) => {
   docClient.scan({TableName: "UserTable"}, function(err, data) {
@@ -45,6 +73,39 @@ app.get('/users/:email', (req, res) => {
   });
 })
 
+app.get('/users/:email/payment-fields', (req, res) => {
+  var params = {
+    TableName: "UserTable",
+    KeyConditionExpression: "#email = :email",
+    ExpressionAttributeNames:{
+        "#email": "email"
+    },
+    ExpressionAttributeValues: {
+        ":email": req.params.email
+    }
+  }
+  docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).send(err)
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data['Items'].length == 0) {
+          res.status(404).send({"msg": "User not found"})
+        } else {
+          var body = {
+            "credit": data['Items'][0]['credit'],
+            "balance": data['Items'][0]['balance'],
+            "queries": data['Items'][0]['queries'],
+          }
+          res.send(body)
+        }
+    }
+  });
+})
+
+
+
 app.post('/users', (req, res) => {
   var params = {
     TableName: "UserTable",
@@ -62,7 +123,7 @@ app.post('/users', (req, res) => {
   // res.send(req.body)
 })
 
-app.put('/users/:email', (req, res) => {
+app.put('/users/:email/activate', (req, res) => {
 
   var params = {
     TableName: "UserTable",
@@ -80,32 +141,119 @@ app.put('/users/:email', (req, res) => {
         res.status(500).send(err)
     } else {
         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-        var user = data['Items'][0]
-        var paramsToUpdate = {
-          TableName: 'UserTable',
-          Key:{
-            "email": user['email'],
-            "registration_date": user['registration_date']
-          },
-          UpdateExpression: "set balance = :balance, user_name=:name, queries=:queries, is_active=:is_active, credit=:credit",
-          ExpressionAttributeValues:{
-              ":credit": req.body['credit'],
-              ":balance": req.body['balance'],
-              ":name": req.body['name'],
-              ":is_active": req.body['is_active'],
-              ":queries": req.body['queries']
-          },
-          ReturnValues:"UPDATED_NEW"
+        if(data['Items'].length == 0) {
+          res.status(404).send({"msg": "User Not Found"})
+        } else {
+          var user = data['Items'][0]
+          user['active'] = 1
+          var result = update_user(user)
+          console.log(result)
+          res.send({"msg": "Activated successfully"})
+          // else res.send({"msg": "Something went wrong"})
         }
-        docClient.update(paramsToUpdate, function(err, data) {
-          if (err) {
-              console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
-              res.status(500).send(err);
+    }
+  });
+})
+
+app.put('/users/:email/deactivate', (req, res) => {
+
+  var params = {
+    TableName: "UserTable",
+    KeyConditionExpression: "#email = :email",
+    ExpressionAttributeNames:{
+        "#email": "email"
+    },
+    ExpressionAttributeValues: {
+        ":email": req.params.email
+    }
+  }
+  docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).send(err)
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data['Items'].length == 0) {
+          res.status(404).send({"msg": "User Not Found"})
+        } else {
+          var user = data['Items'][0]
+          user['active'] = 0
+          var result = update_user(user)
+          console.log(result)
+          res.send({"msg": "Deactivated successfully"})
+          // else res.send({"msg": "Something went wrong"})
+        }
+    }
+  });
+})
+
+app.put('/users/:email/payment-fields/by-admin', (req, res) => {
+
+  var params = {
+    TableName: "UserTable",
+    KeyConditionExpression: "#email = :email",
+    ExpressionAttributeNames:{
+        "#email": "email"
+    },
+    ExpressionAttributeValues: {
+        ":email": req.params.email
+    }
+  }
+  docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).send(err)
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data['Items'].length == 0) {
+          res.status(404).send({"msg": "User Not Found"})
+        } else {
+          var user = data['Items'][0]
+          user['credit'] = req.body['credit']
+          user['balance'] = req.body['balance']
+          var result = update_user(user)
+          console.log(result)
+          res.send({"msg": "Updated successfully"})
+          // else res.send({"msg": "Something went wrong"})
+        }
+    }
+  });
+})
+
+app.put('/users/:email/payment-fields/by-user', (req, res) => {
+
+  var params = {
+    TableName: "UserTable",
+    KeyConditionExpression: "#email = :email",
+    ExpressionAttributeNames:{
+        "#email": "email"
+    },
+    ExpressionAttributeValues: {
+        ":email": req.params.email
+    }
+  }
+  docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).send(err)
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(data['Items'].length == 0) {
+          res.status(404).send({"msg": "User Not Found"})
+        } else {
+          var user = data['Items'][0]
+          if(user['queries'] == 0){
+            res.status(412).send({"msg": "you cannot change your payment info"})
           } else {
-              console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-              res.send({"msg": "Updated successfully"})
+            user['credit'] = req.body['credit']
+            user['balance'] = req.body['balance']
+            user['queries'] -= 1
+            var result = update_user(user)
+            console.log(result)
+            res.send({"msg": "Updated successfully"})
           }
-        });
+          // else res.send({"msg": "Something went wrong"})
+        }
     }
   });
 })
